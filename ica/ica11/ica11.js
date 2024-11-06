4381604e407fb471f7b63637280972055a6457c1
Jude McCall <jumc4457@colorado.edu>
2024-11-06 16:10:17 -0700

    working on it

--- ica/ica11/ica11.js
+++ ica/ica11/ica11.js
@@ -1,22 +1,44 @@
+let output="";
+
+tellFortune(1, "Thor", "Sweden", "Geoguesser");
+tellFortune(2, "Batman", "Gotham", "DJ");
+tellFortune(15, "Yourself", "The bottom of the ocean", "Blob");
+
+calculateDogAge(4);
+calculateDogAge(100);
+calculateDogAge(0);
+
+reverseNum(123456);
+reverseNum(789);
+
+
+
+
 function tellFortune(numChildren, partnerName, geoLocation, jobTitle) {
-    return "You will be "+jobTitle+" in "+geoLocation+", and married to "+partnerName+" with "+numChildren+" kids";
+    output+="You will be a "+jobTitle+" in "+geoLocation+", and married to "+partnerName+" with "+numChildren+" kid(s)<br>";
 }
 
 function calculateDogAge(age) {
     let dogAge = age*7;
-    return "Your doggie is "+dogAge+" years old in dog years!";
+    output+= "Your doggie is "+dogAge+" years old in dog years!<br>";
 }
 
 function reverseNum(num) {
-    len = String(num).length;
-    newString;
-    for (i=len-1, i<=0, i--) {
+    let len = String(num).length;
+    let newString= "";
+    for (i=len-1; i>=0; i--) {
         newString+=String(num).charAt(i);
     }
-    return Number(newString);
+    output+= newString+"<br>";
 }
 
 function alphabetical(string) {
-    
+    let len = string.length();
+    for (i=0; i<len; i++) {
+        
+    }
+
 }
+
+document.getElementById("output").innerHTML = output;
 
